c9ef5814e911ac0d2bcd731079b0cb98769b1b71
root <logicmoo@gmail.com>
2013-08-18 02:56:05 -0700

    fixed proof output

--- t/sanity_base/test_prolog.sh
+++ t/sanity_base/test_prolog.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
 
-[[ $_ != "$0" ]] && echo "Script is being sourced" && exit 9
+
+SWIPL=swipl
+if [ -z `which swipl` ]; then
+    # default locations on OS X
+    SWIPL=/Applications/SWI-Prolog.app/Contents/MacOS/swipl;
+    if [ ! -e $SWIPL ]; then
+        SWIPL=~/bin/swipl;
+    fi
+    if [ ! -e $SWIPL ]; then
+        echo PFC requires SWI-Prolog. Please download from http://www.swi-prolog.org/
+        exit 1
+    fi
+fi
+
+# Symlink resolution: http://stackoverflow.com/a/697552/726581
+
+# get the absolute path of the executable
+SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH=$SELF_PATH/$(basename -- "$0")
+
+# resolve symlinks
+while [[ -h $SELF_PATH ]]; do
+    # 1) cd to directory of the symlink
+    # 2) cd to the directory of where the symlink points
+    # 3) get the pwd
+    # 4) append the basename
+    DIR=$(dirname -- "$SELF_PATH")
+    SYM=$(readlink "$SELF_PATH")
+    SELF_PATH=$(cd "$DIR" && cd "$(dirname -- "$SYM")" && pwd)/$(basename -- "$SYM")
+done
+
+PATH_TO_ME=`dirname $SELF_PATH`;
+if [ -z "$BIOMAKE_PATH" ]; then
+    BIOMAKE_PATH=$PATH_TO_ME/../prolog;
+fi
+
+#$PATH_TO_ME/swipl_wrap -L0 -G0 -T0 -q -p library=$BIOMAKE_PATH  -g 'assert(biomake_prog("'$0'")),main,halt' -t halt -s $BIOMAKE_PATH/biomake/cli -- "$@"
+
+#[[ $_ != "$0" ]] && echo "Script is being sourced" && exit 9
 
 me="${BASH_SOURCE[${#BASH_SOURCE[@]} - 1]}"
 good_exit=4
