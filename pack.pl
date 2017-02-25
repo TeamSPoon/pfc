419bb24f53deeaffb85105a7259f16f0d0fec156
root <logicmoo@gmail.com>
2017-02-25 08:02:59 -0800

    packify

---
+++ pack.pl
@@ -0,0 +1,11 @@
+name(pfc).
+title('Pfc -- a package for forward chaining in Prolog').
+version('0.0.8').
+download('https://github.com/TeamSPoon/pfc/releases/*.zip').
+author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
+packager( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
+home('https://github.com/TeamSPoon/pfc').
+requires(hook_hybrid).
+requires(clause_attvars).
+autoload(true).
+
