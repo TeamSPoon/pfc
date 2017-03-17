7020e78f02a77a5dc452a0b8fe4e53523313da44
root <logicmoo@gmail.com>
2017-03-16 20:55:11 -0700

    v1.1.114

--- pack.pl
+++ pack.pl
@@ -1,6 +1,6 @@
 name(pfc).
 title('Pfc -- a package for forward chaining in Prolog').
-version('1.1.113').
+version('1.1.114').
 download('https://github.com/TeamSPoon/pfc/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
