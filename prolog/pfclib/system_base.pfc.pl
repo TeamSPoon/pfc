4e1e3333f9e386ddcf8af7e8c23d451f26e04abe
root <logicmoo@gmail.com>
2017-09-30 06:30:29 -0700

    cute puppies 2017-09-30-06:30:28

--- prolog/pfclib/system_base.pfc.pl
+++ prolog/pfclib/system_base.pfc.pl
@@ -5,6 +5,7 @@
 :- ensure_abox(baseKB).
 :- use_module(library(rtrace)).
 :- use_module(library(pfc_lib)).
+:- use_module(library(dictoo)).
 % ensure this file does not get unloaded with mpred_reset
 
 
@@ -572,7 +573,9 @@ never_retract_u(X):- cwc, loop_check(never_retract_u(X,_)).
 :- dynamic(mpred_unload_option/2).
 :- listing(mpred_unload_option/2).
 :- mpred_trace_exec.
+%:- rtrace,trace.
 ==> mpred_unload_option(never,$current_file.value).
+:- nortrace.
 :- mpred_notrace_exec.
 :- listing(mpred_unload_option/2).
 
