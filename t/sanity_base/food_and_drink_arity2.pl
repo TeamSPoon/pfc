11d204ab19914a01adbf9d8ba0d29fbe884eb7cb
Douglas R. Miles <logicmoo@gmail.com>
2018-07-01 22:43:26 -0700

    working_version_of_food_and_drink

--- t/sanity_base/food_and_drink_arity2.pl
+++ t/sanity_base/food_and_drink_arity2.pl
@@ -2,8 +2,8 @@
 
 Joe is takin Sue on a date but he doesnt have enough money to buy them both food and drink
 
-Joe wants food more than a drink.
-Sue wants to get a drink and has no thoughts about food.
+Joe wants food .66 more than a drink .34.
+Sue is on a diet so she wants food at .05 (so must be something else on menu?)
 
 */
 :- use_module(library(clpfd)).
@@ -18,8 +18,8 @@ buyable(A):- cost(A,_).
 % person(P):- wants(P,_,_).
 person(P):- pred(Cn,wants),arg1(Cn,P).
 
-% has(joe,money,40).
-pred(has1,has). arg1(has1,joe). arg2(has1,money). arg3(has1,40).
+% has(joe,money,50).
+pred(has1,has). arg1(has1,joe). arg2(has1,money). arg3(has1,50).
 
 
 % has(joe,food,0).
@@ -32,27 +32,27 @@ pred(has4,has). arg1(has4,sue). arg2(has4,food). arg3(has4,0).
 pred(has5,has). arg1(has5,sue). arg2(has5,drink). arg3(has5,0).
 
 
-% wants(joe,food,0.33).
-pred(wants1,wants). arg1(wants1,joe). arg2(wants1,food). arg3(wants1,33).
-% wants(joe,drink,0.66).
-pred(wants2,wants). arg1(wants2,joe). arg2(wants2,drink). arg3(wants2,66).
-% wants(sue,drink,0.66).
-pred(wants3,wants). arg1(wants3,sue). arg2(wants3,drink). arg3(wants3,66).
+% wants(joe,food,66).
+pred(wants1,wants). arg1(wants1,joe). arg2(wants1,food). arg3(wants1,66).
+% wants(joe,drink,34).
+pred(wants2,wants). arg1(wants2,joe). arg2(wants2,drink). arg3(wants2,34).
+% wants(sue,food,5).
+pred(wants3,wants). arg1(wants3,sue). arg2(wants3,food). arg3(wants3,5).
 
-% This rule helps us infer sue wants  food at .34
+% This rule helps us infer sue wants drink at 66
 % wants(Person,Type,Amount):- dif(Type1,Type2), buyable(Type2), person(Person),wants(Person,Type2,Other), Amount #= 1 - Other.
 pred(rule1,wants).
 arg1(rule1,Person):- arg2(rule1,Type),arg3(rule1,Amount),rule1_wants(Person,Type,Amount).
 arg2(rule1,Type):-   arg1(rule1,Person),arg3(rule1,Amount),rule1_wants(Person,Type,Amount).
 arg3(rule1,Amount):- arg2(rule1,Type), arg1(rule1,Person),rule1_wants(Person,Type,Amount).
 
-rule1_wants(Person,Type1,Amount):-
-  dif(Type1,Type2), 
+rule1_wants(Person,Type1,Amount):- 
   buyable(Type2),
-  person(Person),
-  %wants(Person,Type2,Other),
-  clause(pred(Cn,wants),true),arg1(Cn,Person), arg2(Cn,Type2), clause(arg3(Cn,Other)),
+  clause(pred(Cn,wants),true),arg1(Cn,Person), arg2(Cn,Type2), clause(arg3(Cn,Other)),  
+  dif(Type1,Type2),
+  buyable(Type1),  
   Amount #= 100 - Other.
+
 
 % wants_more(P,Thing1):- dif(Thing1,Thing2), wants(P,Thing1,A1), wants(P,Thing2,A2), A1 #>= A2.
 wants_more(P,Thing1):- 
@@ -68,13 +68,15 @@ do_test :-
    wants_more(P2,Thing2),
    cost(Thing1,Cost1),
    cost(Thing2,Cost2),
-   has(P1,money,Cash),
-   Cash #>= Cost1+Cost2, 
+   pred(F1,has),arg1(F1,P1),arg2(F1,cash),arg3(F1,Cash),
+   Cash #>= Cost1+Cost2,
+   Spent is Cost1+Cost2,
    nl,
-   write([
+   write([spent=Spent,
      orders_for(Thing1,P1),
      orders_for(Thing2,P2)]),nl.
 
 
 :- do_test.
+% [spent=50,orders_for(food,joe),orders_for(drink,sue)]
 
