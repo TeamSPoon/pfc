27e4e92731fde2d32af87893463fc1f582c06a53
Douglas R. Miles <logicmoo@gmail.com>
2018-07-01 22:25:17 -0700

    working_version_of_food_and_drink

--- t/sanity_base/food_and_drink.pl
+++ t/sanity_base/food_and_drink.pl
@@ -6,9 +6,9 @@ Joe wants food more than a drink.
 Sue wants to get a drink and has no thoughts about food.
 
 */
-
 :- use_module(library(clpfd)).
-:- use_module(library(tabling)).
+:- style_check(-discontiguous).
+clause(P):-clause(P,true).
 
 cost(food,35).
 cost(drink,15).
@@ -58,29 +58,5 @@ do_test :-
      orders_for(Thing2,P2)]),nl.
 
 
-
-wants_more_arity_2(P,Thing1):- 
-   dif(Thing1,Thing2),
-   pred(F1,wants),arg1(F1,P),arg2(F1,Thing1),arg3(F1,A1),
-   pred(F2,wants),arg1(F2,P),arg2(F2,Thing2),arg3(F2,A2),
-   A1>A2.
-
-
-
-pred(ClNum,Prd):- clauzes(P,ClNum),functor(P,Prd,_).
-arg1(ClNum,Arg):- clauzes(P,ClNum),arg(1,P,Arg).
-arg2(ClNum,Arg):- clauzes(P,ClNum),arg(2,P,Arg).
-arg3(ClNum,Arg):- clauzes(P,ClNum),arg(3,P,Arg).
-
-clauzes(P,ClNum):- arity_was(F,A),functor(P,F,A),clause(P,true,ClNum).
-
-
-
 :- do_test.
-
-
-
-
-
-
 
