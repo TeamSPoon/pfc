8098847a9487864cc628a94419ff19cf21dc7e44
logicmoo <logicmoo@gmail.com>
2020-06-04 04:32:30 -0700

    regressions

--- t/sanity_base/test_prolog.sh
+++ t/sanity_base/test_prolog.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
 
+sourced=0
+if [ -n "$ZSH_EVAL_CONTEXT" ]; then 
+  case $ZSH_EVAL_CONTEXT in *:file) sourced=1;; esac
+elif [ -n "$KSH_VERSION" ]; then
+  [ "$(cd $(dirname -- $0) && pwd -P)/$(basename -- $0)" != "$(cd $(dirname -- ${.sh.file}) && pwd -P)/$(basename -- ${.sh.file})" ] && sourced=1
+elif [ -n "$BASH_VERSION" ]; then
+  (return 0 2>/dev/null) && sourced=1 
+else # All other shells: examine $0 for known shell binary filenames
+  # Detects `sh` and `dash`; add additional shell filenames as needed.
+  case ${0##*/} in sh|dash) sourced=1;; esac
+fi
+
+if [[ sourced -ne 0 ]] ; then
+   echo "Script is being sourced (bye)"
+   sleep 2
+else
+
 SWIPL=swipl
 if [ -z `which swipl` ]; then
     # default locations on OS X
@@ -178,4 +195,6 @@ for ele2 in "${listOfNames[@]}"
 	done
   done
 exit $exitcode  
+
+fi
 
