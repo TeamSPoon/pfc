2e5c1a404f4fc8f6e83e64a17dc2ad76fbbaa440
logicmoo <logicmoo@gmail.com>
2020-07-21 09:37:42 -0700

    auto-commit Tue 21 Jul 2020 09:37:42 AM PDT

--- prolog/pfc.pl
+++ prolog/pfc.pl
@@ -11,17 +11,10 @@
 
 :- rexport(library(pfc_lib)).
 
-%! hello_there_xref is det.
-% 
-%  This is only seen by XREF 
-%
-hello_there_xref.
-
 :- else.  % \+ current_prolog_flag(xref,true)
 */
 :- if(('$current_source_module'(M),
-      M:use_module( library(logicmoo_utils)),
-      M:use_module( library(pfc_iri_resource)),
+      M:use_module( library(pfc_lib)),
       add_pfc_to_module(M))).
 /*
 :-else. % add_pfc_to_module
@@ -30,12 +23,15 @@ hello_there_xref.
          dmsg(Info),throw(Info))).
 */
 :- endif. % add_pfc_to_module
+:- module(pfc_mod,[hello_there_xref/0]).
+%! hello_there_xref is det.
+% 
+%  This is only seen by XREF 
+%
+hello_there_xref.
 
-:- if((prolog_load_context(source,File),prolog_load_context(file,File))).
- :- module(pfc_mod,[]).
-:- endif. 
 
 %  Makes this load multiple times (per use_module)
-:- prolog_load_context(file,File),unload_file(File).
+:- prolog_load_context(source,File),unload_file(File).
 
 %:- endif. % current_prolog_flag(xref,true)
