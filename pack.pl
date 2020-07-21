5dda8bd048e4b1c4931b668250f83e58124027c3
logicmoo <logicmoo@gmail.com>
2020-07-21 02:30:08 -0700

    requires(dictoo).

--- pack.pl
+++ pack.pl
@@ -1,11 +1,12 @@
 name(pfc).
 title('Pfc -- a package for forward chaining in Prolog').
-version('1.4.111').
+version('1.5.665').
 download('https://github.com/TeamSPoon/pfc/releases/*.zip').
 author( 'Douglas R. Miles', 'logicmoo@gmail.com' ).
 packager( 'TeamSPoon/LogicMoo', 'https://github.com/TeamSPoon/' ).
 home('https://github.com/TeamSPoon/pfc').
 %requires(s_expression).
 requires(logicmoo_utils).
+requires(dictoo).
 autoload(false).
 
