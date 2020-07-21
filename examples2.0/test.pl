629644c9c5650919c4e59d5cfb53c749ba260603
logicmoo <logicmoo@gmail.com>
2020-07-20 18:44:00 -0700

    maybe these updates correctly

---
+++ examples2.0/test.pl
@@ -0,0 +1,24 @@
+%% some simple tests to see if Pfc is working properly
+
+:- use_module(library(pfc)).
+
+
+time(Call,Time) :-
+  statistics(runtime,_),
+  call(Call),
+  statistics(runtime,[_,Time]).
+
+
+test0 :- 
+  ain([(p(X) ==> q),
+       p(1),
+       (p(X), ~r(X) ==> s(X)),
+       (t(X), {X>0} ==> r(X)),
+       (t(X), {X<0} ==> minusr(X)),
+       t(-2),
+       t(1)]).
+
+test1 :-
+  consult('~finin/pfc/kinship.pfc'),
+  consult('~finin/pfc/finin.pfc').
+
